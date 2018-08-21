2585ff8407cc5b1a54a0db9af012954fa3c5dcff
restrada25 <restrad2@hawk.iit.edu>
2018-08-21 13:32:12 -0500

    Updating package-lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
+import { GitSearchUsersService } from './git-search-users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  constructor() {
+
+  }
+  ngOnInit() {
+    // this.GitSearchUsersService.gitSearchUsers('tom').then((response) => {
+    //   alert("Total Users Found: "+response.total_count);
+    // }, (error) => {
+    //   alert("Error: "+error.statusText)
+    // })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GitSearchService } from './git-search.service';
+import { GitSearchUsersService } from './git-search-users.service';
+import { GitSearchComponent } from './git-search/git-search.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'search',
+    redirectTo: '/search/angular/1',
+    pathMatch: 'full'
+  },
+  {
+    path: 'search/:query/:pageNumber',
+    component: GitSearchComponent,
+    data: {
+      title: 'Git Search'
+    }
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [GitSearchService, GitSearchUsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
